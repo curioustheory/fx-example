dfa03c7ef9fb52c257361048a70e60799f79c3be
Tony Wang <tony@curioustheory.com>
2017-12-23 15:10:00 +0200

    added test cases

---
+++ tests/test_util.py
@@ -0,0 +1,15 @@
+import unittest
+from forex import util
+
+
+class TestUtil(unittest.TestCase):
+
+    def test_load_config(self):
+        self.fail("todo...")
+
+    def test_load_data(self):
+        self.fail("todo...")
+
+
+if __name__ == '__main__':
+    unittest.main()
