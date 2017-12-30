8df4f90d46923d8984ca9849d8e9d0e3275dcfb7
Tony Wang <tony@curioustheory.com>
2017-12-30 17:16:18 +0200

    updated config file

--- forex/app.py
+++ forex/app.py
@@ -1,68 +1,55 @@
-import pandas as pd
-
 from forex.eda import ExploratoryDataAnalysis
-from forex.util import load_config, load_data
 from forex.featureengineering import FeatureEngineering
+from forex.modelling import Modelling
+from forex.util import load_config, load_data
 
 
 def run(config_file_path):
-    # try:
-        print("reading configuration file... ")
-        config = load_config(config_file_path=config_file_path)
-        print("loading dataframe...")
+    print("reading configuration file... ")
+    config = load_config(config_file_path=config_file_path)
+    train_input_file_path = config["train_input_file_path"]
+    test_input_file_path = config["test_input_file_path"]
+    input_file_header = config["input_file_header"]
+    process_eda = config["process_eda"].lower() == "true"
+    plot_chart = config["plot_chart"].lower() == "true"
+
+    print("loading dataframe...")
+    print("---------------------------------------------------------------------------------------------")
+    dataframe = load_data(input_file_path=train_input_file_path,
+                          input_file_header=input_file_header)
+    print(dataframe.head())
+    print()
+
+    print("data prep / engineering...")
+    print("\t sort out datetime format and removing volume")
+    print("---------------------------------------------------------------------------------------------")
+    engineering = FeatureEngineering(dataframe)
+    engineering.run()
+    dataframe = engineering.get_dataframe()
+    print(dataframe.head())
+    print()
+
+    if process_eda:
+        print("loading exploratory analysis...")
         print("---------------------------------------------------------------------------------------------")
-        dataframe = load_data(input_file_path=config["input_file_path"],
-                              input_file_header=config["input_file_header"])
-        print(dataframe.head())
+        eda = ExploratoryDataAnalysis(dataframe)
+        eda.run(plot_chart)
         print()
 
-        print("data prep / engineering...")
-        print("\t sort out datetime format and removing volume")
-        print("---------------------------------------------------------------------------------------------")
-        engineering = FeatureEngineering(dataframe)
-        engineering.run()
-        dataframe = engineering.get_dataframe()
-        print(dataframe.head())
-        print()
+    print("modelling...")
+    print("---------------------------------------------------------------------------------------------")
+    modelling = Modelling(dataframe)
+    modelling.run()
+    model = modelling.get_optimum_model()
+    print()
 
-        if config["process_eda"].lower() == "true":
-            print("loading exploratory analysis...")
-            print("---------------------------------------------------------------------------------------------")
-            eda = ExploratoryDataAnalysis(dataframe)
-            eda.run()
-            print()
+    print("making a prediction...")
+    print("---------------------------------------------------------------------------------------------")
+    test_dataframe = load_data(input_file_path=test_input_file_path,
+                               input_file_header=input_file_header)
+    print(test_dataframe.head())
+    print()
 
-        print("modelling...")
-        print("---------------------------------------------------------------------------------------------")
-        if config["cache_data"].lower() == "true":
-            pass
-        print()
-
-        print("validation...")
-        print("---------------------------------------------------------------------------------------------")
-
-
-    # except Exception as e:
-    #     print(e)
-
-
-"""
-        # check feature correlation
-        # TODO:
-
-        print("running models...")
-        print("---------------------------------------------------------------------------------------------")
-        trainer = ModelTrainer(dataframe)
-        trainer.run()
-        model = trainer.get_best_model()
-        print()
-
-        print("validating models on unseen data...")
-        print("---------------------------------------------------------------------------------------------")
-        validator = ModelValidator()
-        validator.run()
-        print()
-"""
 
 if __name__ == "__main__":
     run(config_file_path="../config.json")
