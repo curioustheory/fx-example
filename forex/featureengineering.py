d250eb02d2bc3670aa63c30f541fd7615000e5a9
Tony Wang <tony@curioustheory.com>
2017-12-31 16:56:47 +0200

    added new features

--- forex/featureengineering.py
+++ forex/featureengineering.py
@@ -9,7 +9,6 @@ class FeatureEngineering:
     def run(self):
         self._prep_data()
         self._engineer_features()
-        self._clean()
 
     def _prep_data(self):
         """
@@ -19,26 +18,23 @@ class FeatureEngineering:
         self._dataframe["datetime"] = pd.to_datetime(self._dataframe["date"] + " " + self._dataframe["time"],
                                                      format="%Y.%m.%d %H:%M")
         self._dataframe = self._dataframe.drop(["date", "time", "volume"], axis=1)
-
-    def _clean(self):
-        # remove invariant
-
-        # outlier
-
         # fill na
-        pass
 
     def _engineer_features(self):
+        """
+
+        :return:
+        """
         # percentage changes
         self._dataframe["percentage_change"] = self._dataframe["close"].pct_change()
         # high low spread
         self._dataframe["spread"] = self._dataframe["high"] - self._dataframe["low"]
-
-        # avg. 1 hour
-
-        # avg. 4 hour
-
-        # avg. 12 hour
+        # rolling avg. 1 hour
+        self._dataframe["rolling_mean_1h"] = self._dataframe["close"].rolling(window=60).mean()
+        # rolling avg. 24 hour
+        self._dataframe["rolling_mean_24h"] = self._dataframe["close"].rolling(window=60 * 24).mean()
+        # rolling avg. 7 day
+        self._dataframe["rolling_mean_7d"] = self._dataframe["close"].rolling(window=60 * 24 * 7).mean()
 
     def get_dataframe(self):
         return self._dataframe
