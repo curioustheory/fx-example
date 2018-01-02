45eb5d44dccdd644af655225346729aa40383c15
Tony Wang <tony@curioustheory.com>
2018-01-02 11:00:25 +0200

    clean up

--- forex/app.py
+++ forex/app.py
@@ -7,16 +7,15 @@ from forex.util import load_config, load_data
 def run(config_file_path):
     print("reading configuration file... ")
     config = load_config(config_file_path=config_file_path)
-    train_input_file_path = config["train_input_file_path"]
-    test_input_file_path = config["test_input_file_path"]
+    input_file_path = config["input_file_path"]
     input_file_header = config["input_file_header"]
-    process_eda = config["process_eda"].lower() == "true"
-    plot_chart = config["plot_chart"].lower() == "true"
-    correlation_limit = float(config["correlation_limit"])
+    plot_chart = config["plot_chart"]
+    correlation_limit = config["correlation_limit"]
+    train_size = config["train_size"]
 
     print("loading dataframe...")
     print("---------------------------------------------------------------------------------------------")
-    dataframe = load_data(input_file_path=train_input_file_path,
+    dataframe = load_data(input_file_path=input_file_path,
                           input_file_header=input_file_header)
     print(dataframe.head())
     print()
@@ -30,34 +29,26 @@ def run(config_file_path):
     print(dataframe.head())
     print()
 
-    if process_eda:
-        print("loading exploratory analysis...")
-        print("---------------------------------------------------------------------------------------------")
-        eda = ExploratoryDataAnalysis(dataframe)
-        eda.run(plot_chart)
-        print("no. of rows:", dataframe.shape[0])
-        print("no. of columns:", dataframe.shape[1])
-        print()
-        print(eda.get_summary())
-        print()
-        print("feature correlation...")
-        print(eda.get_feature_correlation())
-        print()
+    print("loading exploratory analysis...")
+    print("---------------------------------------------------------------------------------------------")
+    eda = ExploratoryDataAnalysis(dataframe)
+    eda.run(plot_chart)
+    print("no. of rows:", dataframe.shape[0])
+    print("no. of columns:", dataframe.shape[1])
+    print()
+    print(eda.get_summary())
+    print()
+    print("feature correlation...")
+    print(eda.get_feature_correlation())
+    print()
 
     print("modelling...")
     print("---------------------------------------------------------------------------------------------")
-    modelling = Modelling(dataframe, correlation_limit)
+    modelling = Modelling(dataframe, eda.get_summary(), train_size, correlation_limit)
     modelling.run()
-    model = modelling.get_optimum_model()
-    print()
-
-    print("making a prediction...")
-    print("---------------------------------------------------------------------------------------------")
-    test_dataframe = load_data(input_file_path=test_input_file_path,
-                               input_file_header=input_file_header)
-    print(test_dataframe.head())
     print()
 
 
 if __name__ == "__main__":
+    # if selfarg[]
     run(config_file_path="../config.json")
