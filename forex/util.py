9c0a0dfdffbffb2b47a0adbb8cce82a632aec9ad
Tony Wang <tony@curioustheory.com>
2018-01-03 03:03:27 +0200

    refactored

--- forex/util.py
+++ forex/util.py
@@ -6,6 +6,12 @@ import numpy as np
 
 
 def load_config(config_file_path):
+    """
+    load the json file into a dictionary.
+
+    :param config_file_path: file path
+    :return: dictionary
+    """
     if is_file_exists(config_file_path):
         with open(file=config_file_path) as f:
             return json.loads(f.read())
@@ -14,6 +20,13 @@ def load_config(config_file_path):
 
 
 def load_data(input_file_path, input_file_header):
+    """
+    load the data from the file path.
+
+    :param input_file_path: file path
+    :param input_file_header: csv header
+    :return: pandas dataframe
+    """
     if is_file_exists(input_file_path):
         return pd.read_csv(input_file_path, names=input_file_header)
     else:
@@ -21,10 +34,23 @@ def load_data(input_file_path, input_file_header):
 
 
 def is_file_exists(file_path):
+    """
+    check if the file exist.
+
+    :param file_path: file path
+    :return:
+    """
     return os.path.isfile(file_path)
 
 
 def drop_highly_correlated_features(dataframe, correlation_limit=0.95):
+    """
+    check for highly correlated feature and drop them if exceed the defined limit.
+
+    :param dataframe:
+    :param correlation_limit:
+    :return: pandas dataframe
+    """
     # Create correlation matrix
     corr_matrix = dataframe.corr().abs()
 
@@ -38,3 +64,27 @@ def drop_highly_correlated_features(dataframe, correlation_limit=0.95):
     print("drop highly correlated columns (corr > {}): {}".format(correlation_limit, str(columns_to_drop)))
     print()
     return dataframe.drop(columns_to_drop, axis=1)
+
+
+def split_train_test_data(X, y, train_size=0.7):
+    """
+    splits the data into train and test set
+
+    :return: train and test set
+    """
+    print("splitting train / test set:")
+    train_size = int(len(X) * train_size)
+    X_train, X_test = X[0:train_size], X[train_size:len(X)]
+    y_train, y_test = y[0:train_size], y[train_size:len(y)]
+
+    # reset index for plotting
+    X_train = X_train.reset_index(drop=True)
+    X_test = X_test.reset_index(drop=True)
+    y_train = y_train.reset_index(drop=True)
+    y_test = y_test.reset_index(drop=True)
+
+    print("Total rows: {}".format(len(X)))
+    print("Training rows: {}".format(len(X_train)))
+    print("Testing rows: {}".format(len(X_test)))
+    print()
+    return X_train, y_train, X_test, y_test
